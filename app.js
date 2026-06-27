5e6113fdf54daaabac50a93b2f56389dcf83a05e
jhonsegurag <jhonsegurag@gmail.com>
2026-06-27 01:16:02 -0500

    feat: update app.js

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
     next();
 });
 
